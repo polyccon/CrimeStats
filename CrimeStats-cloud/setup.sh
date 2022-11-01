1bd7752d2c0db50bec4f74813eb819fb7d24a1b4
Constantina Polycarpou <polyccon@gmail.com>
2022-11-01 13:02:50 +0000

    fix: creating the zip file without using zip command was causing errors

--- CrimeStats-cloud/setup.sh
+++ CrimeStats-cloud/setup.sh
@@ -1,5 +1,4 @@
 mkdir package
-touch package/hello-python.zip
 pip3 install --target ./package -r requirements.txt
 cd package
 zip -r hello-python.zip .
@@ -7,4 +6,4 @@ cd ..
 pip3 freeze --path ./package > requirements.txt
 zip -g package/hello-python.zip index.py
 zipinfo package/hello-python.zip
-terraform plan -refresh-only
+terraform plan
