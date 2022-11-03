e717ada4fe360ee4077724f355114fe0d1520b3c
Constantina Polycarpou <polyccon@gmail.com>
2022-11-03 15:12:57 +0000

    refactor: move roles and policies in iam.tf and variables in variables.tf

--- CrimeStats-cloud/main.tf
+++ CrimeStats-cloud/main.tf
@@ -1,52 +1,5 @@
 provider "aws" {
-  region = "eu-west-2"
-}
-
-resource "aws_iam_role" "lambda_role" {
-    name   = "Crime_Stats_Lambda_Function_Role"
-    assume_role_policy = <<EOF
-    {
-    "Version": "2012-10-17",
-    "Statement": [
-    {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-        "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-    }
-    ]
-    }
-    EOF
-}
-
-resource "aws_iam_policy" "iam_policy_for_lambda" {
- 
- name         = "aws_iam_policy_for_terraform_aws_lambda_role"
- path         = "/"
- description  = "AWS IAM Policy for managing aws lambda role"
- policy = <<EOF
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-     "Action": [
-       "logs:CreateLogGroup",
-       "logs:CreateLogStream",
-       "logs:PutLogEvents"
-     ],
-     "Resource": "arn:aws:logs:*:*:*",
-     "Effect": "Allow"
-   }
- ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
-    role        = aws_iam_role.lambda_role.name
-    policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+  region = var.region
 }
 
 resource "aws_lambda_function" "terraform_lambda_func" {
---
+++ CrimeStats-cloud/variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  default     = "eu-west-2"
+  type        = string
+  description = "AWS region to use"
+}
