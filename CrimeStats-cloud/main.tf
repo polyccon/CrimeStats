324f0da49eb81a91841c86a3cf1df9a266a07b0f
Constantina Polycarpou <polyccon@gmail.com>
2022-11-12 19:31:55 +0000

    feat: adds configuration for api gateway

--- CrimeStats-cloud/main.tf
+++ CrimeStats-cloud/main.tf
@@ -2,11 +2,72 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_lambda_function" "terraform_lambda_func" {
-    filename                       = "${path.module}/package/crime-stats.zip"
+resource "aws_lambda_function" "crime-stats" {
+    filename                       = "${path.module}/python/crime-stats.zip"
     function_name                  = "Crime_Stats_Lambda"
     role                           = aws_iam_role.lambda_role.arn
     handler                        = "index.lambda_handler"
     runtime                        = "python3.8"
     depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
+
+resource "aws_apigatewayv2_api" "lambda" {
+  name          = "serverless_lambda_gw"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  name        = "serverless_lambda_stage"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
+
+resource "aws_apigatewayv2_integration" "crime-stats" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  integration_uri    = aws_lambda_function.crime-stats.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "crime-stats" {
+  api_id = aws_apigatewayv2_api.lambda.id
+
+  route_key = "GET /data/{location}"
+  target    = "integrations/${aws_apigatewayv2_integration.crime-stats.id}"
+}
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.crime-stats.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
+}
+
--- CrimeStats-cloud/setup.sh
+++ CrimeStats-cloud/setup.sh
@@ -4,5 +4,5 @@ cd package
 zip -r crime-stats.zip .
 cd ..
 pip3 freeze --path ./package > requirements.txt
-zip -g package/crime-stats.zip index.py
-zipinfo package/crime-stats.zip
+zip -g python/crime-stats.zip index.py
+zipinfo python/crime-stats.zip
