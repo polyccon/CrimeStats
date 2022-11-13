4b5df85fbf55c95065182cc8b5c60a6a1e10625c
Constantina Polycarpou <polyccon@gmail.com>
2022-11-13 14:55:47 +0000

    fix: key error - location not found on the event object directly

--- CrimeStats-cloud/index.py
+++ CrimeStats-cloud/index.py
@@ -1,33 +1,44 @@
 import json
 from collections import defaultdict
 import requests
+import logging
+
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.ERROR)
 
 
 def get_data(location):
-    url_postcode = "http://api.postcodes.io/postcodes/" + location
-    resp_postcode = requests.get(url=url_postcode)
-    ll = json.loads(resp_postcode.text)["result"]
-    latitude = ll["latitude"]
-    longitude = ll["longitude"]
-    url = "https://data.police.uk/api/crimes-street/all-crime?"
-    params = dict(lat=latitude, lng=longitude)
-    resp = requests.get(url=url, params=params)
-    data = json.loads(resp.text)
-
-    d = defaultdict(dict)
-    for item in data:
-        if not d.get(item["category"]):
-            d[item["category"]] = 1
-        else:
-            d[item["category"]] += 1
-
     results_list = []
-    for key, value in d.items():
-        result = {"label": key, "value": value}
-        results_list.append(result)
+    if location:
+        url_postcode = "http://api.postcodes.io/postcodes/" + location
+        resp_postcode = requests.get(url=url_postcode)
+        ll = json.loads(resp_postcode.text)["result"]
+        latitude = ll["latitude"]
+        longitude = ll["longitude"]
+        url = "https://data.police.uk/api/crimes-street/all-crime?"
+        params = dict(lat=latitude, lng=longitude)
+        resp = requests.get(url=url, params=params)
+        data = json.loads(resp.text)
+
+        d = defaultdict(dict)
+        for item in data:
+            if not d.get(item["category"]):
+                d[item["category"]] = 1
+            else:
+                d[item["category"]] += 1
+
+        for key, value in d.items():
+            result = {"label": key, "value": value}
+            results_list.append(result)
     return results_list
 
 
 def lambda_handler(event, context):
-    location = event["location"]
+    LOGGER.info("event", event)
+    location = event.get("location", None)
+    if not location:
+        location = event.get("pathParameters", {}).get("location", None)
+    LOGGER.info("location", location)
+    data = get_data(location)
+    LOGGER.info("data", data)
     return json.loads(json.dumps((get_data(location))))
