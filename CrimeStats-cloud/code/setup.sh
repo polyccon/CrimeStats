b6cb80d263dc4c6de7e7de2e87c3256f6add98e9
Constantina Polycarpou <polyccon@gmail.com>
2022-11-13 15:28:09 +0000

    refactor: moves all the application code files in folder named code

--- CrimeStats-cloud/code/setup.sh
+++ CrimeStats-cloud/code/setup.sh
@@ -4,5 +4,5 @@ cd package
 zip -r crime-stats.zip .
 cd ..
 pip3 freeze --path ./package > requirements.txt
-zip -g code/crime-stats.zip index.py
-zipinfo code/crime-stats.zip
+zip -g zip/crime-stats.zip index.py
+zipinfo zip/crime-stats.zip
