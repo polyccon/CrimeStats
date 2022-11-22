7bbd62fda842958026908e38312fe79f6df14840
Constantina Polycarpou <polyccon@gmail.com>
2022-11-22 12:56:40 +0000

    fix: zip directory needs to be created in bash script

--- cloud/code/setup.sh
+++ cloud/code/setup.sh
@@ -1,8 +1,9 @@
-mkdir package
+mkdir package zip
 pip3 install --target ./package -r requirements.txt
 cd package
 zip -r crime-stats.zip .
 cd ..
+mv package/crime-stats.zip zip/crime-stats.zip
 pip3 freeze --path ./package > requirements.txt
 zip -g zip/crime-stats.zip index.py
 zipinfo zip/crime-stats.zip
