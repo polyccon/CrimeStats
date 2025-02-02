f264de9350fd1114074a14c1e63226aba749e4d7
polyccon <polyccon@gmail.com>
2025-02-02 23:59:51 +0000

    refactor: separate convert data function

--- flask-app/src/views.py
+++ flask-app/src/views.py
@@ -40,6 +40,24 @@ def home():
     return redirect("/")
 
 
+def convert_police_data(data):
+    d = defaultdict(dict)
+    for item in data:
+        if not d.get(item['category']):
+            d[item['category']] = 1
+        else:
+            d[item['category']] += 1
+
+    results_list = []
+    for key, value in d.items():
+        result = {
+            'label': key, 'value': value
+        }
+        results_list.append(result)
+
+    return results_list
+
+
 def get_data(location):
     url_postcode = "http://api.postcodes.io/postcodes/"+location
     resp_postcode = requests.get(url=url_postcode)
@@ -53,21 +71,7 @@ def get_data(location):
     )
     resp = requests.get(url=url, params=params)
     data = json.loads(resp.text)
-
-    d = defaultdict(dict)
-    for item in data:
-        if not d.get(item['category']):
-            d[item['category']] = 1
-        else:
-            d[item['category']] += 1
-
-    results_list = []
-    for key, value in d.items():
-        result = {
-            'label': key, 'value': value
-            }
-        results_list.append(result)
-    return results_list
+    return convert_police_data(data)
 
 
 @app.route('/data/<location>')
