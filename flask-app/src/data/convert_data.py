58187de57ab7701f7a62453ad34d260c5a5c4cfe
polyccon <polyccon@gmail.com>
2025-02-16 00:53:07 +0000

    refactor: method can take argument

--- flask-app/src/data/convert_data.py
+++ flask-app/src/data/convert_data.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import List
 
 from src.services.location_client import LocationClient
 from src.services.police_client import PoliceClient
@@ -20,16 +21,16 @@ class CrimeDataProcessor:
             latitude, longitude = self.location_client.postcode_to_coordinates()
             self.crime_data = self.police_client.get_data_for_coordinates(latitude, longitude)
 
-    def gather_category_data(self):
+    def gather_specific_data(self, key: str) -> List:
         """Helper function to count occurrences of a given key in crime data."""
         self.fetch_crime_data()
 
         d = defaultdict(int)
         for item in self.crime_data:
-            d[item['category']] += 1
+            d[item[key]] += 1
 
         return [{'label': key, 'value': value} for key, value in d.items()]
 
     def get_crime_categories(self):
         """Extracts crime categories."""
-        return self.gather_category_data()
+        return self.gather_specific_data("category")
