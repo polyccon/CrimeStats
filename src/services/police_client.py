cb531d5675ff7cf6b2c742d92ef4e2bfe7e04448
polyccon <polyccon@gmail.com>
2025-03-22 19:15:58 +0000

    feat: adds logging to police client

--- src/services/police_client.py
+++ src/services/police_client.py
@@ -1,5 +1,11 @@
 import json
+import logging
 import requests
+
+
+FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+LOGGER = logging.getLogger(__name__)
 
 
 class PoliceClient:
@@ -7,7 +13,13 @@ class PoliceClient:
         pass
 
     def get_data_for_coordinates(self, latitude, longitude):
-        url = "https://data.police.uk/api/crimes-street/all-crime?"
-        params = dict(lat=latitude, lng=longitude)
-        resp = requests.get(url=url, params=params)
-        return json.loads(resp.text)
+        try:
+            url = f"https://data.police.uk/api/crimes-street/all-crime?"
+
+            params = dict(lat=latitude, lng=longitude)
+            resp = requests.get(url=url, params=params)
+            LOGGER.info(f"police client: {resp.status_code}")
+            return resp.json()
+        except Exception as e:
+            LOGGER.error(f"police client: {str(e)}")
+            return {"error": f"police client: {str(e)}"}
