5ecb4b969b86d1ff9d926864b7c71f3b00309af9
polyccon <polyccon@gmail.com>
2025-03-23 23:59:33 +0000

    chore: remove debug log from police client

--- src/services/police_client.py
+++ src/services/police_client.py
@@ -12,6 +12,7 @@ class PoliceClient:
     def __init__(self):
         pass
 
+    @staticmethod
     def get_data_for_coordinates(self, latitude, longitude):
         try:
             url = f"https://data.police.uk/api/crimes-street/all-crime?"
@@ -19,7 +20,6 @@ class PoliceClient:
             params = dict(lat=latitude, lng=longitude)
             resp = requests.get(url=url, params=params)
             LOGGER.info(f"police client: {resp.status_code}")
-            LOGGER.debug("response", resp.json())
             return resp.json()
         except Exception as e:
             LOGGER.error(f"police client: {str(e)}")
